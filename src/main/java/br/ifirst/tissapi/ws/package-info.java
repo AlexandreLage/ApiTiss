3b54b9c87d46e57982947c05090b91d239876630
Wanderson Pimenta <wdpimenta@gmail.com>
2017-12-16 11:44:26 -0200

    Correção do diretorio de geração das classes

---
+++ src/main/java/br/ifirst/tissapi/ws/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ans.gov.br/padroes/tiss/schemas", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package br.ifirst.tissapi.ws;
